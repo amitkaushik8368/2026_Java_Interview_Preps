ef33e20f61c5efd253303d451842a23ac6859a1f
amitkaushik8368 <amit.kaushik9466@gmail.com>
2026-04-03 14:01:53 +0530

    This is third Commit

---
+++ src/TakeString.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class TakeString {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        String str1 = scanner.nextLine();
+        String str2 = scanner.nextLine();
+        System.out.println("String addition is: " + str1 +  " " + str2);
+
+    }
+}
