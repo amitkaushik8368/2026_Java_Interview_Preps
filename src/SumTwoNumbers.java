575b453092cfea5128c05947d44f6c0be8a0c219
amitkaushik8368 <amit.kaushik9466@gmail.com>
2026-04-03 13:27:17 +0530

    This is second Commit

---
+++ src/SumTwoNumbers.java
@@ -0,0 +1,11 @@
+public class SumTwoNumbers {
+    public static void main(String[] args) {
+
+
+        int x = 3;
+        int y = 8;
+        int sum = x+y;
+        System.out.println("Sum of these two numbers are: " + sum);
+
+    }
+}
