884e105b02f97ca35804a6f1acbb8d3e5bf7e5ec
amitkaushik8368 <amit.kaushik9466@gmail.com>
2026-04-04 14:40:14 +0530

    This is first Commit

--- src/April_03_2026/Imp_NonPrimitiveDatatypes.java
+++ src/April_03_2026/Imp_NonPrimitiveDatatypes.java
@@ -1,7 +1,10 @@
 package April_03_2026;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 /***
- *
+ * <p>
  * Here let's mainly talk about String and Arrays
  *
  */
@@ -9,6 +12,27 @@ package April_03_2026;
 
 public class Imp_NonPrimitiveDatatypes {
     public static void main(String[] args) {
+
+        String str = "My name is Amit Sharma";
+        String str1 = new String("My name is Pratima Kaushik");
+
+        System.out.println(str1);
+        System.out.println(str);
+
+        int[] x = {1,2,3,4,5};
+        System.out.println(Arrays.toString(x));
+
+        Scanner scanner = new Scanner(System.in);
+
+        int[] y = new int[3];
+        System.out.println("Enter all three integers below:");
+        for (int i = 0; i<y.length; i++)
+        {
+            y[i] = scanner.nextInt();
+        }
+
+        System.out.println("Integers you entered: " + Arrays.toString(y));
+
 
     }
 }
