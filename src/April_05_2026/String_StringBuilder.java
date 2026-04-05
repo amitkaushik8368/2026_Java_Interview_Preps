1e5e75c3191992cc4324c1123dab09eb12ea5c8c
amitkaushik8368 <amit.kaushik9466@gmail.com>
2026-04-05 16:50:06 +0530

    This is second Commit

---
+++ src/April_05_2026/String_StringBuilder.java
@@ -0,0 +1,28 @@
+package April_05_2026;
+
+public class String_StringBuilder
+{
+    public static void main(String[] args) {
+        String str = "Pratima Kaushik";
+        String lowerString = str.toLowerCase();
+
+        StringBuilder stringBuilder = new StringBuilder(lowerString);
+        stringBuilder.reverse();
+
+
+        str = str.concat("Amit");
+
+
+        System.out.println(stringBuilder);
+        System.out.println(str);
+
+
+
+
+
+        //System.out.println(stringBuilder);
+
+
+
+    }
+}
