d29c9660323a843b8b81132231939338cf6a3e10
amitkaushik8368 <amit.kaushik9466@gmail.com>
2026-04-05 19:02:17 +0530

    This is fifth Commit

---
+++ src/April_05_2026/LeapYearChecker.java
@@ -0,0 +1,26 @@
+package April_05_2026;
+
+import java.util.Scanner;
+
+/**
+ * program to check for a year being a leap year
+ */
+
+public class LeapYearChecker
+{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the year you want to check: ");
+        int x = scanner.nextInt();
+        if(x%4==0)
+        {
+            if (x%100 == 0)
+            {
+                if (x%400 == 0)
+                    System.out.println("This year is leap year");
+                else
+                    System.out.println("The number is not leap year");
+            }
+        } else System.out.println("The number is not leap year");
+    }
+}
