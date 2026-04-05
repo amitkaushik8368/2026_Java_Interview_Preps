f46535796a2449d01cc3f954c5da71ccb51aa2df
amitkaushik8368 <amit.kaushik9466@gmail.com>
2026-04-05 19:05:02 +0530

    This is sixth Commit

---
+++ src/April_06_2026/CheckLeapYear.java
@@ -0,0 +1,13 @@
+package April_06_2026;
+
+public class CheckLeapYear
+{
+    public static void main(String[] args) {
+
+        /**
+         * Check leap year here through easy way
+         */
+
+
+    }
+}
