2b2a48c1c782d35f6bdd3713be5e04ee3c22b3fb
amitkaushik8368 <amit.kaushik9466@gmail.com>
2026-04-13 23:56:22 +0530

    This is third Commit

---
+++ src/April_13_2026/StringReverse1.java
@@ -0,0 +1,29 @@
+package April_13_2026;
+
+import java.util.Arrays;
+
+public class StringReverse1
+{
+    public static void main(String[] args)
+    {
+        String str = "Amit Sharma";
+        char[] chars = str.toCharArray();
+        int x = 0, y = chars.length-1;
+        while (x<y)
+        {
+            char temp;
+            temp = chars[x];
+            chars[x++] = chars[y];
+            chars[y--] = temp;
+        }
+        System.out.println(chars.toString());
+        System.out.println(Arrays.toString(chars));
+        //String str1 = String.valueOf(chars);
+        String str1 = new String(chars);
+        System.out.println(str1);
+
+
+
+
+    }
+}
