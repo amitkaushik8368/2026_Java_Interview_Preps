2c248056303534d4cc2c3a24b824709db7547e7a
amitkaushik8368 <amit.kaushik9466@gmail.com>
2026-04-14 23:42:50 +0530

    This is second Commit

---
+++ src/April_14_2026/IdentifyDuplicateString1.java
@@ -0,0 +1,37 @@
+package April_14_2026;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+public class IdentifyDuplicateString1
+{
+    public static void main(String[] args)
+    {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        String[] strings = input.split("\\s+");
+        String[] duplicates = new String[strings.length];
+        boolean[] visited = new boolean[strings.length];
+        int index = 0;
+        for (int i=0; i<strings.length; i++)
+        {
+            boolean duplicateFound = false;
+            if (visited[i])
+                continue;
+            for (int j = i+1; j<strings.length; j++)
+            {
+                if (strings[i].equalsIgnoreCase(strings[j]))
+                {
+                    visited[j] = true;
+                    duplicateFound = true;
+                }
+            }
+            if (duplicateFound)
+                duplicates[index++] = strings[i];
+        }
+        for (int i = 0; i<index; i++)
+        {
+            System.out.print(duplicates[i] + " ");
+        }
+    }
+}
