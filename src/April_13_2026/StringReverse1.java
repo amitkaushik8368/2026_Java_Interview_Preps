3ca69328ac8e426a14938a110a7d0ae12434e7ea
amitkaushik8368 <amit.kaushik9466@gmail.com>
2026-04-14 09:47:04 +0530

    This is first Commit

--- src/April_13_2026/StringReverse1.java
+++ src/April_13_2026/StringReverse1.java
@@ -9,21 +9,27 @@ public class StringReverse1
         String str = "Amit Sharma";
         char[] chars = str.toCharArray();
         int x = 0, y = chars.length-1;
-        while (x<y)
+        for(; x<y; x++, y--)
         {
             char temp;
             temp = chars[x];
-            chars[x++] = chars[y];
-            chars[y--] = temp;
+            chars[x] = chars[y];
+            chars[y] = temp;
         }
-        System.out.println(chars.toString());
+//
+//        while (x<y)
+//        {
+//            char temp;
+//            temp = chars[x];
+//            chars[x++] = chars[y];
+//            chars[y--] = temp;
+//        }
+
+
+        //System.out.println(chars.toString());
         System.out.println(Arrays.toString(chars));
         //String str1 = String.valueOf(chars);
         String str1 = new String(chars);
         System.out.println(str1);
-
-
-
-
     }
 }
---
+++ src/April_14_2026/IdentifyDuplicateString.java
@@ -0,0 +1,41 @@
+package April_14_2026;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class IdentifyDuplicateString
+{
+    public static void main(String[] args)
+    {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+       // String lowerInput = input.toLowerCase();
+        String[] strings = input.split("\\s+");
+        List<String> duplicateStrings = new ArrayList<>();
+        List<Integer> duplicatesAt = new ArrayList<>();
+
+
+        for (int i = 0; i<strings.length-1; i++)
+        {
+            boolean duplicateFound= false;
+            if (duplicatesAt.contains(i))
+            {
+                continue;
+            }
+            for(int j = i+1 ; j<strings.length; j++)
+            {
+                if (strings[i].equalsIgnoreCase(strings[j]))
+                {
+                    duplicatesAt.add(j);
+                    duplicateFound = true;
+                }
+            }
+            if (duplicateFound)
+                duplicateStrings.add(strings[i]);
+        }
+        System.out.println("Duplicate Strings: " + duplicateStrings);
+
+
+    }
+}
