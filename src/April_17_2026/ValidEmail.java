3764e307c0bb4a49690eeaf51c81bbdadd3d5cc3
amitkaushik8368 <amit.kaushik9466@gmail.com>
2026-04-17 09:52:49 +0530

    This is third Commit

---
+++ src/April_17_2026/ValidEmail.java
@@ -0,0 +1,24 @@
+package April_17_2026;
+
+import java.util.Scanner;
+
+public class ValidEmail
+{
+    /**
+     *  amit@live.com - Valid
+     *  amit@dasda - Not valid
+     */
+
+    public static void main(String[] args)
+    {
+        System.out.println("Enter the email ID: ");
+        Scanner scanner = new Scanner(System.in);
+        String emailID = scanner.nextLine();
+        scanner.close();
+
+
+
+
+
+    }
+}
