c25b4d1fbac53c26d74ce68228e376876da90966
amitkaushik8368 <amit.kaushik9466@gmail.com>
2026-04-22 23:56:09 +0530

    This is fourth Commit

--- src/April_22_2026/ArrayElementFrequency2.java
+++ src/April_22_2026/ArrayElementFrequency2.java
@@ -1,8 +1,11 @@
 package April_22_2026;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class ArrayElementFrequency2
 {
@@ -12,17 +15,16 @@ public class ArrayElementFrequency2
          * Find the frequency of each element in the array
          */
         int[] array = {9, 23, 54, 9, 34, 54, 23, 94, 123, 5, 8, 9, 8};
+         Map<Integer, Long> freq =
+                 Arrays.stream(array)
+                         .boxed()
+                         .collect(Collectors.groupingBy(
+                                 Function.identity(),
+                                 Collectors.counting()
+                         ));
+         System.out.println(freq);
 
-        /**
-         * Map<Integer, Long> freq =
-         *     Arrays.stream(array)
-         *           .boxed()
-         *           .collect(Collectors.groupingBy(
-         *               Function.identity(),
-         *               Collectors.counting()
-         *           ));
-         *
-         * System.out.println(freq);
+         /**
          *
          * -----------------------------------------------
          * Arrays.sort(array);
---
+++ src/April_22_2026/DuplicateElements.java
@@ -0,0 +1,11 @@
+package April_22_2026;
+
+public class DuplicateElements
+{
+    public static void main(String[] args)
+    {
+        // Identify duplicate elements in an array
+        int[] array = {9, 23, 54, 9, 34, 54, 23, 94, 123, 5, 8, 9, 8};
+
+    }
+}
