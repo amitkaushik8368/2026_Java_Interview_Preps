2e40dcd364d0a7dd30c4dd397f08d16d2aa097d8
amitkaushik8368 <amit.kaushik9466@gmail.com>
2026-04-23 10:04:55 +0530

    This is first Commit

--- src/April_22_2026/DuplicateElements.java
+++ src/April_22_2026/DuplicateElements.java
@@ -1,11 +1,75 @@
 package April_22_2026;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 public class DuplicateElements
 {
     public static void main(String[] args)
     {
-        // Identify duplicate elements in an array
-        int[] array = {9, 23, 54, 9, 34, 54, 23, 94, 123, 5, 8, 9, 8};
+        //Approach 1
+        int[] arr = {5, 8, 9, 34, 29, 8, 6, 5, 9, 29, 8, 34};
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int a: arr)
+            map.put(a, map.getOrDefault(a, 0) +1);
+        System.out.println(map);
+
+        for(Map.Entry<Integer, Integer> entry : map.entrySet())
+        {
+            if(entry.getValue() > 1)
+                System.out.printf("%d ",entry.getKey());
+        }
+
+		/*
+		Approach 2:
+
+		int[] arr = {5, 8, 9, 34, 29, 8, 6, 5, 9, 29, 8, 34};
+		//int[] duplicates = new int[arr.length];
+		//int indexDuplicates = 0;
+		List<Integer> duplicates = new ArrayList<>();
+		boolean[] visited = new boolean[arr.length];
+		for(int i=0; i < arr.length ; i++)
+		{
+			int count = 1;
+			if(visited[i])
+				continue;
+			for(int j=i+1; j < arr.length ; j++)
+			{
+				if(arr[i] == arr[j])
+				{
+					visited[j] = true;
+					count++;
+				}
+			}
+			if(count>1)
+			{
+				duplicates.add(arr[i]);
+				//duplicates[indexDuplicates++] = arr[i];
+			}
+		}
+		//int[] newArray = Arrays.copyOf(duplicates, indexDuplicates);
+		//System.out.println(Arrays.toString(newArray));
+		System.out.println(duplicates);
+
+	*/
+
+
+	/*
+
+	Approach 3:
+
+		int[] arr = {5, 8, 9, 34, 29, 8, 6, 5, 9, 29, 8, 34};
+		Set<Integer> filterDuplicates = new HashSet<>();
+		Set<Integer> duplicates = new HashSet<>();
+		for(int a: arr)
+		{
+			if(!filterDuplicates.add(a))
+				duplicates.add(a);
+		}
+		System.out.println(duplicates);
+
+	*/
 
     }
 }
