58a76350e509de4ed8b5aa2d5b72459dfbfd9ab8
amitkaushik8368 <amit.kaushik9466@gmail.com>
2026-04-29 09:40:52 +0530

    This is first Commit

---
+++ src/April_29_2026/ThreadSynchronization2.java
@@ -0,0 +1,33 @@
+package April_29_2026;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+class Counter1{
+    AtomicInteger count = new AtomicInteger();
+    void increment()
+    {
+        count.incrementAndGet();
+    }
+}
+public class ThreadSynchronization2
+{
+    public static void main(String[] args) throws InterruptedException {
+        Counter1 counter = new Counter1();
+        Thread t1 = new Thread(
+                () -> {
+                        for (int i = 0; i < 10_000; i++)
+                            counter.increment();
+                    });
+        Thread t2 = new Thread(
+                () -> {
+                    for (int i = 0; i < 10_000; i++)
+                        counter.increment();
+                }
+        );
+        t1.start();
+        t2.start();
+        t1.join();
+        t2.join();
+        System.out.println(counter.count);
+    }
+}
