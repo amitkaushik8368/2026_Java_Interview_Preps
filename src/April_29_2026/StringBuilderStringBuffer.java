a7d759d08ab3746b6b9fef038354ae87205309f7
amitkaushik8368 <amit.kaushik9466@gmail.com>
2026-04-29 21:43:51 +0530

    This is third Commit

--- src/April_29_2026/StringBuilderStringBuffer.java
+++ src/April_29_2026/StringBuilderStringBuffer.java
@@ -2,16 +2,23 @@ package April_29_2026;
 
 public class StringBuilderStringBuffer
 {
-    private final static int ITERATIONS = 100_000;
+    private final static int ITERATIONS = 10_000_000;
     public static void main(String[] args)
     {
         long start = System.currentTimeMillis();
-        System.out.println(start);
+        //System.out.println(start);
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < ITERATIONS; i++) {
             sb.append("a");
         }
         long end = System.currentTimeMillis();
         System.out.println("StringBuilder time with single thread: " + (end-start));
+        start = System.currentTimeMillis();
+        StringBuffer sbf = new StringBuffer();
+        for (int i = 0; i < ITERATIONS; i++) {
+            sbf.append("a");
+        }
+        end = System.currentTimeMillis();
+        System.out.println("StringBuffer time with single thread: " + (end-start));
     }
 }
