631f89be31a0601b596192917753d9c7671e00d7
amitkaushik8368 <amit.kaushik9466@gmail.com>
2026-05-03 23:02:05 +0530

    This is fifth Commit

---
+++ src/May_2026/May_3/SecondLargestArrayElement.java
@@ -0,0 +1,27 @@
+package May_2026.May_3;
+
+public class SecondLargestArrayElement
+{
+    public static void main(String[] args) {
+        int[] array = {5, 6, 8, 8, 3, 1, 9, 9};
+        int largest = Integer.MIN_VALUE;
+        int secondLargest = Integer.MIN_VALUE;
+        for (int element: array)
+        {
+            if (element>secondLargest)
+                secondLargest=element;
+            System.out.println("Second Largest 1st if = " + secondLargest );
+            if (largest<secondLargest)
+            {
+                int temp = largest;
+                largest = secondLargest;
+                secondLargest = temp;
+            }
+            System.out.println("Second Largest 2nd if = " + secondLargest);
+            System.out.println("Largest 2nd if = " + largest);
+            System.out.println("======================");
+
+        }
+        System.out.println(secondLargest);
+    }
+}
