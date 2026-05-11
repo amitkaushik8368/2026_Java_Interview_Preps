cc2a851fa37e5f2bae105dfcfe21fe5822d8dc2b
amitkaushik8368 <amit.kaushik9466@gmail.com>
2026-05-11 09:49:16 +0530

    This is second Commit

--- src/May_2026/Week2/MoveZeroes.java
+++ src/May_2026/Week2/MoveZeroes.java
@@ -4,5 +4,13 @@ public class MoveZeroes
 {
     public static void main(String[] args) {
         int[] arr = {4, 0, 5, 0, 3, 0, 9};
+        for (int i = arr.length-1; i >=0 ; i--) {
+
+            if (arr[i] == 0 && i < arr.length-1 && arr[i+1] !=0)
+            {
+
+            }
+
+        }
     }
 }
