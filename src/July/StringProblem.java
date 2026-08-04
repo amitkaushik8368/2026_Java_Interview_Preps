c27ac95fd540ba6275d98c473e49651c9b0d4299
amitkaushik8368 <amit.kaushik9466@gmail.com>
2026-08-04 09:59:03 +0530

    This is first Commit

--- src/July/StringProblem.java
+++ src/July/StringProblem.java
@@ -7,14 +7,17 @@ public class StringProblem
     public static void main(String[] args)
     {
         String str = "I love pune,pune is great punepunepune";
-        String str1 = "punepunepune";
         String input = "pune";
-        String[] strArray = str.split(input, -1);
-        int count = strArray.length-1;
-        System.out.println(Arrays.toString(strArray));
-        System.out.println(strArray.length);
-
-
+//        String[] strArray = str.split(input, -1);
+//        System.out.println(strArray.length-1);
+        int index = 0;
+        int count = 0;
+        while ((index=str.indexOf(input, index)) != -1)
+        {
+            index = index+input.length();
+            count++;
+        }
+        System.out.println("Count of 'pune' : " + count);
 
     }
 }
